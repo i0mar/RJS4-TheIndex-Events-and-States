8e06c8a6bcb8c815cc162cad304102a3df98d252
Omar Tawfik <xbox.ps3.psp@hotmail.com>
2020-01-11 19:25:15 +0300

    Task Completed.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 // Data
 import authors from "./data";
@@ -6,20 +6,42 @@ import authors from "./data";
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
+import AuthorDetail from './AuthorDetail';
 
-function App() {
-  return (
-    <div id="app" className="container-fluid">
-      <div className="row">
-        <div className="col-2">
-          <Sidebar />
-        </div>
-        <div className="content col-10">
-          <AuthorsList authors={authors} />
+class App extends Component {
+  state = {
+    currentAuthor: null
+  };
+
+  selectAuthor = author => {
+    this.setState({currentAuthor: author});
+  };
+
+  unSelectAuthor = () => {
+    this.setState({currentAuthor: null});
+  };
+
+  isAuthorSelected = () => {
+    if (this.state.currentAuthor)
+      return <AuthorDetail author={this.state.currentAuthor}/>;
+    else
+      return <AuthorsList authors={authors} func={this.selectAuthor} />;
+  }
+
+  render () {
+    return (
+      <div id="app" className="container-fluid">
+        <div className="row">
+          <div className="col-2">
+            <Sidebar func={this.unSelectAuthor}/>
+          </div>
+          <div className="content col-10">
+            {this.isAuthorSelected()}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/AuthorsList.js
+++ src/AuthorsList.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
 
 // Components
 import AuthorCard from "./AuthorCard";
 
-function AuthorsList(props) {
-  const authorCards = props.authors.map(author => (
-    <AuthorCard key={author.first_name + author.last_name} author={author} />
+class AuthorsList extends Component {
+  state = {
+    authorsToList: this.props.authors
+  };
+
+  authorCards = this.state.authorsToList.map(author => (
+    <AuthorCard key={author.first_name + author.last_name} author={author} func={this.props.func}/>
   ));
 
-  return (
-    <div className="authors">
-      <h3>Authors</h3>
-      <div className="row">{authorCards}</div>
-    </div>
-  );
+  filterAuthors = query => {
+    let temp = this.props.authors.filter(author => (author.first_name + ' ' + author.last_name).toUpperCase().includes(query.toUpperCase()));
+    this.setState({authorsToList: temp});
+
+    this.authorCards = this.state.authorsToList.map(author => (
+      <AuthorCard key={author.first_name + author.last_name} author={author} func={this.props.func}/>
+    ));  
+  };
+z
+  render() {
+    return (
+      <div className="authors">
+        <SearchBar func={this.filterAuthors}/>
+        <h3>Authors</h3>
+        <div className="row">{this.authorCards}</div>
+      </div>
+    );
+  };  
 }
 
 export default AuthorsList;
